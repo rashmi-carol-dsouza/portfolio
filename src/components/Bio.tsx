dfdbfbfe5c54c85c69172cc5583faee5629a744f
Larry D Almeida <hello@larrydalmeida.com>
2023-04-15 17:41:47 +0200

    Fix CLS (#16)
    * Fix CLS
    
    * Fix linting error
    
    * Fix export linting error

---
+++ src/components/Bio.tsx
@@ -0,0 +1,12 @@
+const Bio = () => {
+    return (
+      <div className="my-5 max-w-[600px] text-neutral-800">
+        <p>
+          I enable businesses to make data driven decisions with my skills in
+          Python, data analysis, Machine Learning and Computer Vision.
+        </p>
+      </div>
+    );
+  };
+
+export default Bio;
