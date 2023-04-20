d9cb0b435de9adb1cb351c9f177702e0e9b665de
Larry D Almeida <hello@larrydalmeida.com>
2023-04-20 23:27:26 +0200

    Fix shimmer effect

--- src/app/about/Stats.tsx
+++ src/app/about/Stats.tsx
@@ -9,12 +9,12 @@ const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
+      <stop stop-color="#f8edeb" offset="20%" />
+      <stop stop-color="#fae1dd" offset="50%" />
+      <stop stop-color="#ffe5d9" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
+  <rect width="${w}" height="${h}" fill="#ffe5d9" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
@@ -68,7 +68,7 @@ const getImageProps = ({
   src: imageUrl,
   className: "my-0 mx-auto",
   placeholder: blur ? 'blur' as PlaceholderValue : undefined,
-  blurDataURL: blur ? `data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}` : undefined,
+  blurDataURL: blur ? `data:image/svg+xml;base64,${toBase64(shimmer(width, height))}` : undefined,
 });
 
 const AchievementImage = (props: AchievementProps) => {
